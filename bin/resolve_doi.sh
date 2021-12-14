fe9dca5173453a809db7edaedf7986e6905f6161
Tazro Inutano Ohta <inutano@gmail.com>
2021-12-14 17:15:36 +0900

    remove spaces and intermediate newlines

--- bin/resolve_doi.sh
+++ bin/resolve_doi.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 STYLE="american-medical-association-no-et-al"
 cat "${1}" | while read doi; do
-  curl -s -LH "Accept: text/x-bibliography; style=${STYLE}" "https://doi.org/${doi}"
+  curl -s -LH "Accept: text/x-bibliography; style=${STYLE}" "https://doi.org/${doi}" | \
+    sed -e 's:^ *::g' | \
+    tr -d '\n' | \
+    awk '$0'
   sleep 1
 done
