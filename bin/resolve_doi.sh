4cd06da2f7e2ad97a5cd6ca0dd8e7698fa7096a0
Tazro Inutano Ohta <inutano@gmail.com>
2022-09-03 01:59:38 +0900

    make it bold

--- bin/resolve_doi.sh
+++ bin/resolve_doi.sh
@@ -4,6 +4,7 @@ cat "${1}" | while read doi; do
   curl -s -LH "Accept: text/x-bibliography; style=${STYLE}" "https://doi.org/${doi}" | \
     sed -e 's:^ *::g' | \
     tr -d '\n' | \
-    awk '$0'
+    awk '$0' | \
+    sed -e 's:Ohta T:**Ohta T**:g'
   sleep 1
 done
